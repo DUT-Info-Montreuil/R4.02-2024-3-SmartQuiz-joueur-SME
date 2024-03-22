0c5c5b661f2dcb37d720582fd915c8abdffc65a6
rboulkriat <rababboulkriat@outlook.fr>
2024-03-22 11:30:53 +0100

    ajout des tests pour l'ajout du joueur

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/SmartQuiz/exceptions/DonneesSaisiesIncorrectesException.java
@@ -0,0 +1,8 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.exceptions;
+
+public class DonneesSaisiesIncorrectesException extends Exception {
+    public DonneesSaisiesIncorrectesException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/SmartQuiz/exceptions/PseudoNonUniqueException.java
@@ -0,0 +1,7 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.exceptions;
+
+public class PseudoNonUniqueException extends Exception {
+    public PseudoNonUniqueException(String message) {
+        super(message);
+    }
+}
