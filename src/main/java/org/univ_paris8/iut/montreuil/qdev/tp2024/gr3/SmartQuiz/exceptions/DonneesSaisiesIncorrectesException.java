2988db933895150e1e311b3f6f535d86a612e4aa
YoannGit <lehongcheffsonyoann@gmail.com>
2024-03-22 11:25:59 +0100

    ajout de l'interface pour les IServicesJoueur, les exceptions et la classe d'implémentation.

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/SmartQuiz/exceptions/DonneesSaisiesIncorrectesException.java
@@ -0,0 +1,7 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.exceptions;
+
+public class DonneesSaisiesIncorrectesException extends Exception {
+    public DonneesSaisiesIncorrectesException(String message) {
+        super(message);
+    }
+}
